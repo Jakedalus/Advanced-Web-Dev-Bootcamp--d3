d2c3e3a739394c6b93e2038e48c244739c4486fd
Jakedalus <jacquedalus@gmail.com>
2019-06-18 14:31:11 -0400

    Add remove functionality

--- notes-app-source-code/app.js
+++ notes-app-source-code/app.js
@@ -1,7 +1,7 @@
 var typing = false;
 
-d3.select("#new-note")
-    .on('submit', function() {
+d3.select("#submit-btn")
+    .on('click', function() {
       d3.event.preventDefault();
       const input = d3.select('input');
       const typingNote = d3.select('.typing');
@@ -29,3 +29,33 @@ d3.select('#new-note input')
       
     });
     
+
+d3.select('#remove-btn')
+    .on('click', function() {
+      d3.selectAll('.note').remove();
+    });
+
+d3.select('#lucky-btn')
+    .on('click', function() {
+      console.log(d3.selectAll('.note'));
+      console.log(d3.selectAll('.note').nodes());
+      d3.selectAll('.note').nodes().forEach(note => {
+        console.log(note);    
+        console.log(d3.select(note));    
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        const randomFontSize = Math.floor(Math.random()*50);
+        console.log(randomColor);
+        console.log(d3.select(note).style('background-color'));
+        console.log(d3.select(note).style('font-size'));
+        d3.select(note).style('background-color', randomColor + ' !important');
+        d3.select(note).style('font-size', randomFontSize + ' !important');
+      });
+    });
+
+
+
+
+
+
+
+
