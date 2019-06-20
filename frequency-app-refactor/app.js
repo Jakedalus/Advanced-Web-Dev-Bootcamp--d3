7cd1f3ab92eb8b69faec36aa869dfd0e08e7e8bb
Jakedalus <jacquedalus@gmail.com>
2019-06-20 15:40:17 -0400

    Add my solution to svg & d3 exercise

---
+++ frequency-app-refactor/app.js
@@ -0,0 +1,88 @@
+var width = 400;
+var height = 200;
+
+
+d3.select('#reset')
+  .on('click', function() {
+    d3.selectAll(".letter")
+      .remove();
+  
+    d3.select('#phrase')
+      .text(``);
+  
+    d3.select('#count')
+      .text(``);
+});
+
+d3.select('form')
+  .on('submit', function() {
+    d3.event.preventDefault();
+    const inputField = d3.select('input');
+    const input = inputField.property('value');
+    console.log("Submitting...");
+    console.log(input);
+    const data = analyzeInput(input);
+    console.log(data);
+  
+    var numBars = data.length;
+    var barPadding = 10;
+    var barWidth = (width / numBars) - barPadding;
+    console.log(numBars, barWidth);
+    
+    const letters = d3.select('#letters')
+              .attr('width', width)
+              .attr('height', height)
+              .selectAll('.letter')
+              .data(data, d => d.char);
+    letters
+      .classed('new', false)
+      .exit()
+      .remove();
+  
+    letters
+      .enter()
+      .append('rect')
+//        .text(d => `${d.char}` )
+        .classed('letter', true)
+        .classed('new', true)
+        .attr('width', barWidth)
+        .attr('height', d => d.count * 100)
+        .attr('y', d => height - (d.count * 100) )
+        .attr('x', (d,i) => (barWidth + barPadding) * i)
+      .append('text')
+        .text(d => `${d.char}` )
+        .attr('y', d => height - (d.count * 100) )
+        .attr('x', (d,i) => (barWidth + barPadding) * i)
+      .merge(letters)
+        .style('height', d => ((d.count / input.length) * 400) + 'px');
+  
+    d3.select('#phrase')
+      .text(`Analysis of ${input}`);
+  
+    d3.select('#count')
+      .text(`New characters: ${letters.enter().nodes().length}`);
+  
+    inputField.property('value', '');
+  });
+
+function analyzeInput(str) {
+  const freq = {};
+//  str = str.replace(/\s/g, '');
+  const arr = str.split("").sort();
+  console.log(arr);
+  for (c of arr) {
+    if (freq.hasOwnProperty(c)) {
+      freq[c]++;
+    } else {
+      freq[c] = 1;
+    }
+  }
+  const data = [];
+  for (o in freq) {
+    data.push({
+      char: o,
+      count: freq[o]
+    });
+  }
+  return data;
+}
