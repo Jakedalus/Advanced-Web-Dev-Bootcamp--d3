ae57294ce0b81068992299747769fb14fad8430f
Jakedalus <jacquedalus@gmail.com>
2019-06-23 15:19:34 -0400

    Get basic pie chart rendering for exercise

---
+++ pie_chart_exercise/app.js
@@ -0,0 +1,53 @@
+var minYear = d3.min(birthData, d => d.year);
+var maxYear = d3.max(birthData, d => d.year);
+var width = 600;
+var height = 600;
+
+var months = Array.from(new Set(birthData.map(d => d.month)));
+
+var monthColorScale = d3.scaleOrdinal()
+						.domain(months)
+						.range(d3.schemeCategory20);
+
+d3.select('svg')
+	.attr('width', width)
+	.attr('height', height)
+ .append('g')
+	.attr('transform', `translate(${width/2}, ${height/2})`)
+	.classed('chart', true);
+
+makeMonthGraph(minYear);
+
+function makeMonthGraph(year) {
+	var yearData = birthData.filter(d => d.year === year);
+	
+	
+	var arcs = d3.pie()
+				.value(d => d.births)
+				.sort((a, b) => {
+					if (months.indexOf(a.month) < months.indexOf(b.month)) return -1;
+					else return 1;
+				})
+				(yearData);
+	
+	var path = d3.arc()
+				.outerRadius((width / 2) - 10)
+				.innerRadius(width / 4);
+	
+	var update = d3.select('.chart')
+					.selectAll('.arc')
+					.data(arcs);
+	
+	update
+		.exit()
+		.remove();
+	
+	update
+		.enter()
+		.append('path')
+			.classed('arc', true)
+		.merge(update)
+			.attr('fill', d => monthColorScale(d.data.month))
+			.attr('stroke', 'black')
+			.attr('d', path);
+}
