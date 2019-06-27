58c267d5e02ef90c58ae1e031f4b158db4a985b2
Jakedalus <jacquedalus@gmail.com>
2019-06-27 16:15:50 -0400

    Finish up map example

--- d3-map-starter/app.js
+++ d3-map-starter/app.js
@@ -21,6 +21,79 @@ d3.queue()
 			countries.forEach(country => country.properties = row);
 		});
 
-		console.log(populationData);
+		console.log(geoData);
+
+		var width = 700;
+		var height = 500;
+
+		var projection = d3.geoEquirectangular()
+												.scale(110)
+												.translate([width / 2, height / 2.4]);
+		// var projection = d3.geoAzimuthalEqualArea();
+
+		var path = d3.geoPath()	
+									.projection(projection);
+
+
+
+		d3.select('svg')
+				.attr('width', width)
+				.attr('height', height)
+			.selectAll('.country')
+			.data(geoData)
+			.enter()
+			.append('path')
+				.classed('country', true)
+				.attr('d', path);
+
+
+		var select = d3.select('select');
+
+		select
+			.on('change', d => setColor(d3.event.target.value));
+
+		setColor(select.property('value'));
+
+
+		function setColor(val) {
+
+			console.log(val);
+
+			var colorRange = {
+				population: ['white', 'purple'],
+				populationDensity: ['white', 'red'],
+				medianAge: ['white', 'blue'],
+				fertilityRate: ['white', 'orange']
+			};
+
+			var scale = d3.scaleLinear()
+										.domain([0, d3.max(populationData, d => d[val])])
+										.range(colorRange[val]);
+
+			d3.selectAll('.country')
+					.transition()
+					.duration(750)
+					.ease(d3.easeBackIn)
+					.attr('fill', d => {
+						var data = d.properties[val];
+
+						return data ? scale(data) : '#ccc';
+					});
+
+		}
 
 	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
