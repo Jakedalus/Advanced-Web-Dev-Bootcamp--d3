d4b100d67535ada109b029bb0a5a3c1737cb5f42
Jakedalus <jacquedalus@gmail.com>
2019-06-29 14:54:51 -0400

    Load in map data, add co2 data to geodata

--- co2-dashboard-project/app.js
+++ co2-dashboard-project/app.js
@@ -1,27 +1,39 @@
-d3.csv('./data/all_data.csv', function(error, data) {
-	if (error) throw error;
+d3.queue()
+	.defer(d3.json, '//unpkg.com/world-atlas@1.1.4/world/50m.json')
+	.defer(d3.csv, './data/all_data.csv')
+	.await((error, mapData, co2Data) => {
+		if (error) throw error;
 
-	console.log(data);
+		var geoData = topojson.feature(mapData, mapData.objects.countries).features;
 
+		console.log(mapData);
+		console.log(co2Data);
 
-	// var mapWidth = 700;
-	// var mapHeight = 700;
+		co2Data.forEach(row => {
+			var countries = geoData.filter(d => d.id === row['Country Code']);
+			countries.forEach(country => country.properties = row);
+		});
 
-	var map = d3.select('#map');
-				// .attr('width', mapWidth)
-				// .attr('height', mapHeight);
-
-	// var chartWidth = 500;
-	// var chartHeight = 500;
-
-	var pie = d3.select('#pie');
-				// .attr('width', chartWidth)
-				// .attr('height', chartHeight);
+		console.log(geoData);
 
 
-	var bar = d3.select('#bar');
-				// .attr('width', chartWidth)
-				// .attr('height', chartHeight);
+		// var mapWidth = 700;
+		// var mapHeight = 700;
 
-	
-});
+		var map = d3.select('#map');
+					// .attr('width', mapWidth)
+					// .attr('height', mapHeight);
+
+		// var chartWidth = 500;
+		// var chartHeight = 500;
+
+		var pie = d3.select('#pie');
+					// .attr('width', chartWidth)
+					// .attr('height', chartHeight);
+
+
+		var bar = d3.select('#bar');
+					// .attr('width', chartWidth)
+					// .attr('height', chartHeight);
+	});
+
