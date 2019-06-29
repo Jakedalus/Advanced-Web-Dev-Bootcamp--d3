cfccf58307f3b1ae9fec1723fe9fbdcd61104f8f
Jakedalus <jacquedalus@gmail.com>
2019-06-29 18:56:58 -0400

    Fix up year slider for map, everything working now

--- co2-dashboard-project/app.js
+++ co2-dashboard-project/app.js
@@ -4,21 +4,63 @@ d3.queue()
 	.await((error, mapData, co2Data) => {
 		if (error) throw error;
 
-		var geoData = topojson.feature(mapData, mapData.objects.countries).features;
+
+		// LOAD AND PROCESS DATA
+
+		
 
 		console.log('mapData:', mapData);
 		console.log('co2Data:', co2Data);
 
-		var filteredData = co2Data.filter(d => +d.Year === 2011);
+		var geoData = topojson.feature(mapData, mapData.objects.countries).features;
 
-		console.log('filteredData:', filteredData);
-
-		filteredData.forEach(row => {
+		co2Data.forEach(row => {
 			var countries = geoData.filter(d => d.id === row['Country Code']);
-			countries.forEach(country => country.properties = row);
+			countries.forEach(country => {
+				// console.log('row:', row);
+				if (country.properties.data) {
+					country.properties.data.push({
+						year: row.Year,
+						emisssions: row.Emissions,
+						emisssionsPerCapita: row['Emissions Per Capita']
+					}); 
+				} else {
+					country.properties.data = [{
+						year: row.Year,
+						emisssions: row.Emissions,
+						emisssionsPerCapita: row['Emissions Per Capita']
+					}];
+					country.properties.country = row.Country;
+					country.properties.continent = row.Continent;
+					country.properties.countryCode = row['Country Code'];
+					country.properties.region = row.Region;
+				}
+					
+			});
 		});
 
 		console.log('geoData:', geoData);
+
+		
+
+		// YEAR INPUT SLIDER
+
+		d3.select('#year-input')
+			.property('min', d3.min(co2Data, d => +d.Year))
+			.property('max', d3.max(co2Data, d => +d.Year))
+			.property('value', d3.max(co2Data, d => +d.Year))
+			.on('change', function() {
+				console.log('Setting year to: ', +d3.event.target.value);
+
+				d3.select('#current-year')
+					.text(+d3.event.target.value);
+
+				setMapColor(+d3.event.target.value);
+			});
+
+
+
+		// MAP
 
 		var map = d3.select('#map');
 
@@ -44,40 +86,79 @@ d3.queue()
 				.classed('country', true)
 				.attr('d', path);
 
+
 		setMapColor(2011);
 
-		function setMapColor() {
+		d3.select('#current-year')
+					.text(2011);
 
-			
+
+		// function setFilteredData(year) {
+		// 	var geoData = topojson.feature(mapData, mapData.objects.countries).features;
+		// 	var filteredCO2Data = co2Data.filter(d => +d.Year === year);
+
+		// 	console.log('filteredCO2Data:', filteredCO2Data);
+
+		// 	filteredCO2Data.forEach(row => {
+		// 		var countries = geoData.filter(d => d.id === row['Country Code']);
+		// 		countries.forEach(country => country.properties = row);
+		// 	});
+
+		// 	console.log('geoData:', geoData);
+
+		// 	return geoData;
+
+		// }
+
+		function setMapColor(year) {
+
+			console.log('Year:', year);
+
+			var filteredCO2Data = co2Data.filter(d => +d.Year === year);
+
+			console.log('filteredCO2Data:', filteredCO2Data);
 
 			// console.log('Data filtered by year:', filteredData);
-			console.log('Max Emissions:', d3.max(geoData, d => +d.properties.Emissions));
-			console.log('Max Emissions Per Capita:', d3.max(geoData, d => +d.properties['Emissions Per Capita']));
-			console.log('Max Emissions Per Capita:', geoData.filter(d => +d.properties['Emissions Per Capita'] === d3.max(geoData, d => +d.properties['Emissions Per Capita'])));
+			console.log('Max Emissions:', d3.max(filteredCO2Data, d => +d.Emissions));
+			console.log('Max Emissions Per Capita:', d3.max(filteredCO2Data, d => +d['Emissions Per Capita']));
+			console.log('Max Emissions Per Capita:', filteredCO2Data.filter(d => +d['Emissions Per Capita'] === d3.max(filteredCO2Data, d => +d['Emissions Per Capita'])));
 
-			console.log('Min Emissions Per Capita:', d3.min(geoData, d => +d.properties['Emissions Per Capita']));
+			console.log('Min Emissions Per Capita:', d3.min(filteredCO2Data, d => +d['Emissions Per Capita']));
 
 
 			var emissionsScale = d3.scaleLinear()
-							.domain([0, d3.max(geoData, d => +d.properties.Emissions)])
+							.domain([0, d3.max(filteredCO2Data, d => +d.Emissions)])
 							.range(['white', '#bb0a1e']);
 
 			var emissionsPerCapitaScale = d3.scaleLinear()
-							.domain([0, d3.max(geoData, d => +d.properties['Emissions Per Capita']) ])
+							.domain([0, d3.max(filteredCO2Data, d => +d['Emissions Per Capita']) ])
 							.range(['orange','darkred']);
+
+			console.log(d3.selectAll('.country'));
 
 			d3.selectAll('.country')
 					.transition()
 					.duration(750)
 					.ease(d3.easeBackIn)
 					.attr('fill', d => {
-						// console.log(d);
-						// var data = d.properties.Emissions;
-						var data = +d.properties['Emissions Per Capita'];
+						console.log('setting country color:', d.properties.country);
+						console.log(d);
+						var data = null;
+						if(Object.entries(d.properties).length !== 0 && d.properties.constructor === Object) {
+							data = d.properties.data.filter(d => +d.year === year)[0];
 
-						console.log(d.properties.Country, data, emissionsPerCapitaScale(data));
+							console.log('data:', data);
 
-						return data ? emissionsPerCapitaScale(data) : '#ccc';
+							if (data) console.log(d.properties.country, data.emisssionsPerCapita, emissionsPerCapitaScale(data.emisssionsPerCapita));
+
+							return data ? emissionsPerCapitaScale(data.emisssionsPerCapita) : '#ccc';
+						}
+
+						return '#ccc';
+
+						
+
+						// return data ? emissionsPerCapitaScale(data) : '#ccc';
 						// return data ? emissionsScale(data) : '#ccc';	
 					});
 		}
